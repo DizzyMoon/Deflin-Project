6131b6cc570e0a9aec334d14bd575134b2077e9d
DizzyMoon <mikkel.simons1@gmail.com>
2022-05-11 11:33:32 +0200

    Added Member and Result

---
+++ src/Member.java
@@ -0,0 +1,89 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Date;
+
+abstract class Member {
+    private String name;
+    private String memberID;
+    private final Date birth;
+    private boolean competitive;
+    private double restance;
+    private ArrayList<Result> butterflyResults;
+    private ArrayList<Result> crawlResults;
+    private ArrayList<Result> rygcrawlResults;
+    private ArrayList<Result> breastStroke;
+
+    public Member(String name, Date birth, boolean competitive) {
+        this.name = name;
+        this.birth = birth;
+        this.competitive = competitive;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setMemberID(String memberID) {
+        this.memberID = memberID;
+    }
+
+    public Date getBirth() {
+        return birth;
+    }
+
+    public void setCompetitive(boolean competitive) {
+        this.competitive = competitive;
+    }
+
+    public void setRestance(double restance) {
+        this.restance = restance;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getMemberID() {
+        return memberID;
+    }
+
+    public String getCompetitive() {
+        return competitive ? "Competitive member" : "Exercise member";
+    }
+
+    public double getRestance() {
+        return restance;
+    }
+
+    public ArrayList<Result> getButterflyResults() {
+        return butterflyResults;
+    }
+
+    public void setButterflyResults(ArrayList<Result> butterflyResults) {
+        this.butterflyResults = butterflyResults;
+    }
+
+    public ArrayList<Result> getCrawlResults() {
+        return crawlResults;
+    }
+
+    public void setCrawlResults(ArrayList<Result> crawlResults) {
+        this.crawlResults = crawlResults;
+    }
+
+    public ArrayList<Result> getRygcrawlResults() {
+        return rygcrawlResults;
+    }
+
+    public void setRygcrawlResults(ArrayList<Result> rygcrawlResults) {
+        this.rygcrawlResults = rygcrawlResults;
+    }
+
+    public ArrayList<Result> getBreastStroke() {
+        return breastStroke;
+    }
+
+    public void setBreastStroke(ArrayList<Result> breastStroke) {
+        this.breastStroke = breastStroke;
+    }
+}
---
+++ src/Result.java
@@ -0,0 +1,27 @@
+import java.util.Date;
+
+public class Result {
+    private double resultTime;
+    private Date date;
+
+    public Result(double resultTime, Date date){
+        this.resultTime = resultTime;
+        this.date = date;
+    }
+
+    public double getResultTime(){
+        return resultTime;
+    }
+
+    public Date getDate(){
+        return date;
+    }
+
+    public void setResultTime(double resultTime){
+        this.resultTime = resultTime;
+    }
+
+    public void setDate(Date date){
+        this.date = date;
+    }
+}
