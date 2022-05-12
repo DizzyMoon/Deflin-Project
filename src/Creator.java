bcce2d5e05b1451b7c233c6e2e34c81af0693dc1
DizzyMoon <mikkel.simons1@gmail.com>
2022-05-12 11:07:48 +0200

    Removed nested class in Creator

--- src/Creator.java
+++ src/Creator.java
@@ -5,11 +5,7 @@ import members.Member;
 public class Creator {
   private MemberList ml = new MemberList();
 
-  public class UserIDGenerator {
-
-    private UserIDGenerator() { }
-
-    public static String giveUserID() {
+    public String giveUserID() {
       UUID u = UUID.randomUUID();
       return toIDString(u.getMostSignificantBits()) + toIDString(u.getLeastSignificantBits());
     }
@@ -31,7 +27,7 @@ public class Creator {
         '0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',
         'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',
     };
-  }
+
   public void addMember(Member member){
     ml.getList().add(member);
   }
