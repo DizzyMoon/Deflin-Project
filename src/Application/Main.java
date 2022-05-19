fa77e3ffee9d9e0a7fb01f635d519a76ff2735da
ThomasSA-Git <thom45w9@stud.kea.dk>
2022-05-19 12:30:46 +0200

    Comparator til Achievement lavet

---
+++ src/Application/Main.java
@@ -0,0 +1,9 @@
+package Application;
+
+import java.io.FileNotFoundException;
+
+public class Main {
+  public static void main(String[] args) throws FileNotFoundException {
+    new Controller().run();
+  }
+}
