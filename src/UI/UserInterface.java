3860562702b58a16bee643796ac3a9425a20cfb1
ThomasSA-Git <thom45w9@stud.kea.dk>
2022-05-19 20:38:07 +0200

    Top 5 userInterface Sorteringsmetoder for junior og senior Opdateret medlemstabel

--- src/UI/UserInterface.java
+++ src/UI/UserInterface.java
@@ -273,7 +273,7 @@ public class UserInterface {
       System.out.print(space + ml.getList().get(i).getCrawlResults().get(1) + space.repeat(9) + line);
 
       //Tredjebedste tid
-      System.out.print(space + ml.getList().get(i).getCrawlResults().get(2) + space.repeat(9) + line);
+      System.out.print(space + ml.getList().get(i).getCrawlResults().get(2) + space.repeat(11) + line);
 
       //Linjeadskillelse
       System.out.println(underLine.repeat(128));
