f8e03c7217efd072fe55c80a2be15d01f26eb2af
ThomasSA-Git <thom45w9@stud.kea.dk>
2022-05-26 14:38:18 +0200

    ikdsfl

---
+++ test/Application/ControllerTest2.java
@@ -0,0 +1,15 @@
+/*
+package Application;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ControllerTest {
+
+  @org.junit.jupiter.api.Test
+  void subscription() {
+  }
+
+  @org.junit.jupiter.api.Test
+  void subscriptionIncome() {
+  }
+}*/
